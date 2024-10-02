d0efef4701102c8faea97e649be59f090e612ada
Lewis Hazell <git@hazell.dev>
2024-10-02 20:45:28 +0100

    fix: fixed empty input case

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -4,6 +4,11 @@ public class StringCalculator
 {
     public int Add(string input)
     {
+        if (input == string.Empty)
+        {
+            return 0;
+        }
+
         return int.Parse(input);
     }
 }
