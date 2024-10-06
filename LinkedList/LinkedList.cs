01a3f08de16fb0dc50729133503d6a719a794573
Lewis Hazell <git@hazell.dev>
2024-10-06 15:20:46 +0100

    feat: implemented two item functionality

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -1,27 +1,51 @@
-﻿namespace LinkedList;
+﻿using System.Text;
+
+namespace LinkedList;
 
 public class LinkedList<T>
 {
+    private readonly StringBuilder stringBuilder = new();
     private Node? head;
 
     public void Insert(int index, T value)
     {
-        if (index != 0)
+        if (index == 0)
+        {
+            head = new() { Value = value };
+        }
+        else if (index == 1 && head is not null)
+        {
+            head.Next = new() { Value = value };
+        }
+        else
         {
             throw new IndexOutOfRangeException();
         }
-
-        head = new() { Value = value };
     }
 
     public string PrintList()
     {
-        return head?.ToString() ?? string.Empty;
+        stringBuilder.Clear();
+
+        if (head is not null)
+        {
+            stringBuilder.Append(head.ToString());
+
+            if (head.Next is not null)
+            {
+                stringBuilder.Append(',');
+                stringBuilder.Append(head.Next.ToString());
+            }
+        }
+
+        return stringBuilder.ToString();
     }
 
     private class Node
     {
         public required T Value { get; set; }
+
+        public Node? Next { get; set; }
 
         public override string? ToString() => Value?.ToString();
     }
