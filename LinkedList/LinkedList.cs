6b60af14168e986a4afc2193bfd3cd67ed693704
Lewis Hazell <git@hazell.dev>
2024-10-06 15:14:34 +0100

    feat: implemented single item functionality

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -2,8 +2,27 @@
 
 public class LinkedList<T>
 {
+    private Node? head;
+
+    public void Insert(int index, T value)
+    {
+        if (index != 0)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        head = new() { Value = value };
+    }
+
     public string PrintList()
     {
-        return string.Empty;
+        return head?.ToString() ?? string.Empty;
+    }
+
+    private class Node
+    {
+        public required T Value { get; set; }
+
+        public override string? ToString() => Value?.ToString();
     }
 }
